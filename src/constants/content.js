4c3f56bdc3d6a436767c726dda2031157f0e7476
neeraj-dev-007 <sangwan.neeraj2022@gmail.com>
2024-04-10 22:37:36 +0530

    chore: update website content

--- src/constants/content.js
+++ src/constants/content.js
@@ -1,67 +1,46 @@
 
-export const HERO_CONTENT = `I am a passionate software engineer with a knack for crafting robust and scalable web applications. With 2 years of hands-on experience, I have honed my skills in front-end technologies like React, as well as back-end technologies like Node.js, Express.js, Java, Springboot, MySQL and MongoDB. My goal is to leverage my expertise to create innovative solutions that drive business growth and deliver exceptional user experiences.`;
+export const HERO_CONTENT = ` I'm a skilled software engineer with 2 years of experience, specializing in React for front-end and Node.js, Express.js, Java, Spring Boot, MySQL, and MongoDB for back-end development. My goal is to create innovative solutions that drive business growth and deliver exceptional user experiences. Experienced in Jenkins pipeline and Docker CI/CD, I ensure smooth project deployment and delivery. Let's connect and explore opportunities together!`;
 
-export const ABOUT_TEXT = `I am a dedicated and versatile full stack developer with a passion for creating efficient and user-friendly web applications. With 5 years of professional experience, I have worked with a variety of technologies, including React, Next.js, Node.js, MySQL, PostgreSQL, and MongoDB. My journey in web development began with a deep curiosity for how things work, and it has evolved into a career where I continuously strive to learn and adapt to new challenges. I thrive in collaborative environments and enjoy solving complex problems to deliver high-quality solutions. Outside of coding, I enjoy staying active, exploring new technologies, and contributing to open-source projects.`;
+export const ABOUT_TEXT = `I am a dedicated and versatile full stack developer with a passion for creating efficient and user-friendly web applications. With more than 2 years of professional experience, I have worked with a variety of technologies, including React, Java, Spring Boot, Node.js, Express.js, Python, MySQL, and MongoDB. My journey in web development began with a deep curiosity for how things work, and it has evolved into a career where I continuously strive to learn and adapt to new challenges. I thrive in collaborative environments and enjoy solving complex problems to deliver high-quality solutions.`;
 
 export const EXPERIENCES = [
   {
-    year: "2023 - Present",
-    role: "Senior Full Stack Developer",
-    company: "Google Inc.",
-    description: `Led a team in developing and maintaining web applications using JavaScript, React.js, and Node.js. Implemented RESTful APIs and integrated with MongoDB databases. Collaborated with stakeholders to define project requirements and timelines.`,
-    technologies: ["Javascript", "React.js", "Next.js", "mongoDB"],
+    year: "Jan 2024 - Present",
+    role: "Software Engineer - II",
+    company: "Zenon Analytics",
+    description: `Developed and designed a B2B SaaS application for Master Data Management. Developed user interfaces using React.js and implemented back-end RESTful APIs using Java, Springboot and integrated with MySQL database. Collaborated with stakeholders to define project requirements and delivered withing the specified timelines.`,
+    technologies: ["Typescript", "React.js", "Java", "Springboot", "MySQL"],
   },
   {
-    year: "2022 - 2023",
-    role: "Frontend Developer",
-    company: "Adobe",
-    description: `Designed and developed user interfaces for web applications using Next.js and React. Worked closely with backend developers to integrate frontend components with Node.js APIs. Implemented responsive designs and optimized frontend performance.`,
-    technologies: ["HTML", "CSS", "Vue.js", "mySQL"],
+    year: "July 2022 - Dec 2023",
+    role: "Software Engineer - I",
+    company: "Zenon Analytics",
+    description: `Developed a Hiring and Recruitment Portal. Implemented back-end RESTful APIs using Java, Springboot and integrated with MySQL database. Implemented Eureka Server, API Gateway to support load balancing and routing between microservices.`,
+    technologies: ["Java", "Springboot", "MySQL", "Docker"],
   },
   {
-    year: "2021 - 2022",
-    role: "Full Stack Developer",
-    company: "Facebook",
-    description: `Developed and maintained web applications using JavaScript, React.js, and Node.js. Designed and implemented RESTful APIs for data communication. Collaborated with cross-functional teams to deliver high-quality software products on schedule.`,
-    technologies: ["Python", "Svelte", "Three.js", "Postgres"],
-  },
-  {
-    year: "2020 - 2021",
-    role: "Software Engineer",
-    company: "Paypal",
-    description: `Contributed to the development of web applications using JavaScript, React.js, and Node.js. Managed databases and implemented data storage solutions using MongoDB. Worked closely with product managers to prioritize features and enhancements.`,
-    technologies: ["Ruby", "Rails", "PHP", "Sqlite"],
+    year: "Jan 2022 - June 2022",
+    role: "Software Engineer Intern",
+    company: "Zenon Analytics",
+    description: `Designed and implemented RESTful APIs using Java, Springboot and integrated with MySQL database. Implemented Python script for extracting contract details from excel workbooks by using Pandas.`,
+    technologies: ["Java", "Springboot", "MySQL", "Python", "Pandas", "Docker"],
   },
 ];
 
 // export const PROJECTS = [
 //   {
-//     title: "E-Commerce Website",
-//     image: project1,
+//     title: "BookYourRoom",
+//     image: bookYourRoom,
 //     description:
-//       "A fully functional e-commerce website with features like product listing, shopping cart, and user authentication.",
-//     technologies: ["HTML", "CSS", "React", "Node.js", "MongoDB"],
+//       "A fully functional hotel booking website with features like creating hotel, booking, payment thorugh Stripe and user authentication.",
+//     technologies: ["Typescript", "React", "Tailwind CSS", "Node.js", "Express.js", "MongoDB"],
 //   },
 //   {
-//     title: "Task Management App",
-//     image: project2,
-//     description:
-//       "An application for managing tasks and projects, with features such as task creation, assignment, and progress tracking.",
-//     technologies: ["HTML", "CSS", "Angular", "Firebase"],
-//   },
-//   {
-//     title: "Portfolio Website",
-//     image: project3,
+//     title: "Portfolio website",
+//     image: portfolio,
 //     description:
 //       "A personal portfolio website showcasing projects, skills, and contact information.",
-//     technologies: ["HTML", "CSS", "React", "Bootstrap"],
-//   },
-//   {
-//     title: "Blogging Platform",
-//     image: project4,
-//     description:
-//       "A platform for creating and publishing blog posts, with features like rich text editing, commenting, and user profiles.",
-//     technologies: ["HTML", "CSS", "Vue.js", "Express", "mySQL"],
+//     technologies: ["Javascript", "React", "Tailwind CSS", "Framer Motion"],
 //   },
 // ];
 
