e9d0bad3368a03ff3562c4fc626c20ab14404a92
neeraj-dev-007 <sangwan.neeraj2022@gmail.com>
2024-10-31 13:24:52 +0530

    feat: update technologies

--- src/components/Technologies.jsx
+++ src/components/Technologies.jsx
@@ -1,100 +1,99 @@
 import { RiReactjsLine } from "react-icons/ri";
-import { SiMongodb } from "react-icons/si";
+import { SiGo, SiMongodb } from "react-icons/si";
 import { FaNodeJs } from "react-icons/fa";
 import { GrMysql } from "react-icons/gr";
-import { SiExpress } from "react-icons/si";
 import { BiLogoSpringBoot } from "react-icons/bi";
 import { FaJava } from "react-icons/fa";
-import { SiTailwindcss } from "react-icons/si";
 import { motion } from "framer-motion";
- 
+
 const iconVariants = (duration) => ({
-    initial: { y:-10 },
-    animate: {
-        y: [10, -10],
-        transition: {
-            duration: duration,
-            ease: "linear",
-            repeat: Infinity,
-            repeatType: "reverse"
-        }
-    }
-})
+  initial: { y: -10 },
+  animate: {
+    y: [10, -10],
+    transition: {
+      duration: duration,
+      ease: "linear",
+      repeat: Infinity,
+      repeatType: "reverse",
+    },
+  },
+});
 
 const Technologies = () => {
-
-    return (
-        <div className="border-b border-neutral-800 pb-24">
-            <motion.h1 
-            whileInView={{opacity:1, y:0}}
-            initial={{opacity:0, y:-100}}
-            transition={{duration:0.5}}
-            className="my-20 text-center text-4xl">
-                Technologies
-            </motion.h1>
-            <motion.div
-            whileInView={{opacity:1, x:0}}
-            initial={{opacity:0, x:-100}}
-            transition={{duration:1.5}}
-            className="flex flex-wrap items-center justify-center gap-4">
-                <motion.div 
-                variants = {iconVariants(2.5)}
-                initial = "initial"
-                animate = "animate"
-                className="rounded-2xl border-4 border-neutral-800 p-4">   
-                    <SiTailwindcss className="text-4xl text-cyan-600"/>
-                </motion.div>
-                <motion.div 
-                variants = {iconVariants(2)}
-                initial = "initial"
-                animate = "animate"
-                className="rounded-2xl border-4 border-neutral-800 p-4">
-                    <SiMongodb className="text-4xl text-green-500"/>
-                </motion.div>
-                <motion.div 
-                variants = {iconVariants(4)}
-                initial = "initial"
-                animate = "animate"
-                className="rounded-2xl border-4 border-neutral-800 p-4">   
-                    <SiExpress className="text-4xl text-white"/>
-                </motion.div>
-                <motion.div 
-                variants = {iconVariants(3)}
-                initial = "initial"
-                animate = "animate"
-                className="rounded-2xl border-4 border-neutral-800 p-4">
-                    <RiReactjsLine className="text-4xl text-cyan-400"/>
-                </motion.div>
-                <motion.div 
-                variants = {iconVariants(3.5)}
-                initial = "initial"
-                animate = "animate"
-                className="rounded-2xl border-4 border-neutral-800 p-4">   
-                    <FaNodeJs className="text-4xl text-green-500"/>
-                </motion.div>
-                <motion.div 
-                variants = {iconVariants(2.5)}
-                initial = "initial"
-                animate = "animate"
-                className="rounded-2xl border-4 border-neutral-800 p-4">   
-                    <GrMysql className="text-4xl text-cyan-600"/>
-                </motion.div>
-                <motion.div 
-                variants = {iconVariants(2)}
-                initial = "initial"
-                animate = "animate"
-                className="rounded-2xl border-4 border-neutral-800 p-4">   
-                    <FaJava className="text-4xl text-red-600"/>
-                </motion.div>
-                <motion.div 
-                variants = {iconVariants(3)}
-                initial = "initial"
-                animate = "animate"
-                className="rounded-2xl border-4 border-neutral-800 p-4">   
-                    <BiLogoSpringBoot className="text-4xl text-green-600"/>
-                </motion.div>
-            </motion.div>
-        </div>
-    );
+  return (
+    <div className="border-b border-neutral-800 pb-24">
+      <motion.h1
+        whileInView={{ opacity: 1, y: 0 }}
+        initial={{ opacity: 0, y: -100 }}
+        transition={{ duration: 0.5 }}
+        className="my-20 text-center text-4xl"
+      >
+        Technologies
+      </motion.h1>
+      <motion.div
+        whileInView={{ opacity: 1, x: 0 }}
+        initial={{ opacity: 0, x: -100 }}
+        transition={{ duration: 1.5 }}
+        className="flex flex-wrap items-center justify-center gap-4"
+      >
+        <motion.div
+          variants={iconVariants(2.5)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
+          <SiGo className="text-4xl text-cyan-600" />
+        </motion.div>
+        <motion.div
+          variants={iconVariants(2)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
+          <FaJava className="text-4xl text-red-600" />
+        </motion.div>
+        <motion.div
+          variants={iconVariants(3.5)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
+          <FaNodeJs className="text-4xl text-green-500" />
+        </motion.div>
+        <motion.div
+          variants={iconVariants(3)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
+          <BiLogoSpringBoot className="text-4xl text-green-600" />
+        </motion.div>
+        <motion.div
+          variants={iconVariants(3)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
+          <RiReactjsLine className="text-4xl text-cyan-400" />
+        </motion.div>
+        <motion.div
+          variants={iconVariants(2.5)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
+          <GrMysql className="text-4xl text-cyan-600" />
+        </motion.div>
+        <motion.div
+          variants={iconVariants(2)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
+          <SiMongodb className="text-4xl text-green-500" />
+        </motion.div>
+      </motion.div>
+    </div>
+  );
 };
 export default Technologies;
